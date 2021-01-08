bd416efe683a2df7a1638e7eb5f9582b9bc7fe43
peachberry <peachberry0318@gmail.com>
2021-01-08 23:45:44 +0900

    Queue interface and QueueImpl implementation is complete

--- Queue/src/Queue.java
+++ Queue/src/Queue.java
@@ -1,3 +1,32 @@
-public interface Queue {
+public interface Queue<T> {
 
+    /**
+     * 가득찼는지 확인하는 메서드
+     * @return 가득찼다면 ? true : false
+     */
+    Boolean isFull();
+
+    /**
+     * 비었는지 확인하는 메서드
+     * @return 비었다면 ? true : false
+     */
+    Boolean isEmpty();
+
+    /**
+     * Queue에 데이터를 삽입하는 메서드
+     * @param data 삽입할 데이터
+     */
+    void push(T data); //
+
+    /**
+     * Queue에 데이터를 빼내는 메서드
+     * @return 제일 먼저 들어온 데이터 제거(O)
+     */
+    T poll();
+
+    /**
+     * Queue의 제일 먼저 빠져야할 데이터를 확인하는 메서드
+     * @return 제일 먼저 들어온 데이터 제거(X)
+     */
+    T peek();
 }
--- Queue/src/QueueImpl.java
+++ Queue/src/QueueImpl.java
@@ -1,2 +1,61 @@
-public class QueueImpl {
+import java.util.logging.Logger;
+
+public class QueueImpl implements Queue<Integer>{
+
+    private Integer[] store;
+    private Integer front;
+    private Integer back;
+    private Integer size;
+    private Logger logger = Logger.getGlobal();
+
+    public QueueImpl(int length) {
+        this.store = new Integer[length];
+        this.front = 0;
+        this.back = 0;
+        this.size = 0;
+    }
+
+    @Override
+    public Boolean isFull() {
+        return store.length == size;
+    }
+
+    @Override
+    public Boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void push(Integer data) {
+        if(!isFull()) {
+            store[back++] = data;
+            back = back%store.length;
+            size++;
+        }
+        else {
+            logger.warning("queue full");
+        }
+    }
+
+    @Override
+    public Integer poll() {
+        if(!isEmpty()) {
+            Integer data = store[front++];
+            front = front%store.length;
+            size--;
+            return data;
+        }
+        else {
+            logger.warning("queue empty");
+            return null;
+        }
+    }
+
+    @Override
+    public Integer peek() {
+        if(!isEmpty())
+            return store[front];
+        else
+            return null;
+    }
 }

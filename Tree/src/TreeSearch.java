c451e0d0db0da833c388d715f848fcfebdc99f15
peachberry <peachberry0318@gmail.com>
2021-01-11 22:12:44 +0900

    Tree source code revise

---
+++ Tree/src/TreeSearch.java
@@ -0,0 +1,31 @@
+/**
+ * Tree를 탐색할때에 필요한 메서드들을 작성한 인터페이스 입니다
+ */
+public interface TreeSearch {
+    /**
+     * 전위 순회를 할때 사용되는 메서드
+     *
+     * @param node 최상위 root 노드이다
+     */
+    static void preOrder(TreeNode node) {
+        
+    }
+
+    /**
+     * 중위 순회를 할때 사용되는 메서드
+     * @param node 최상위 root 노드
+     */
+    static void inOrder(TreeNode node) {
+
+    }
+
+    /**
+     * 후위 순회를 할때 사용되는 메서드
+     * @param node 최상위 root 노드
+     */
+    static void postOrder(TreeNode node) {
+
+    }
+
+
+}

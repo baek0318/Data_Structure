d09d2b5032adcbae606468db00502a37c6232002
peachberry <peachberry0318@gmail.com>
2021-01-17 19:46:38 +0900

    Fix TreeSearch Interface for test

--- Tree/src/TreeSearch.java
+++ Tree/src/TreeSearch.java
@@ -1,46 +1,27 @@
+import java.util.ArrayList;
+
 /**
  * Tree를 탐색할때에 필요한 메서드들을 작성한 인터페이스 입니다
  */
 public interface TreeSearch {
+
     /**
      * 전위 순회를 할때 사용되는 메서드
-     *
      * @param node 최상위 root 노드이다
      */
-    static void preOrder(TreeNode node) {
-        if(node == null)
-            return;
-
-        System.out.println(node.getData());
-        preOrder(node.getLeft());
-        preOrder(node.getRight());
-    }
+    public void preOrder(TreeNode node);
 
     /**
      * 중위 순회를 할때 사용되는 메서드
      * @param node 최상위 root 노드
      */
-    static void inOrder(TreeNode node) {
-        if(node == null)
-            return;
-
-        inOrder(node.getLeft());
-        System.out.println(node.getData());
-        inOrder(node.getRight());
-    }
+    public void inOrder(TreeNode node);
 
     /**
      * 후위 순회를 할때 사용되는 메서드
      * @param node 최상위 root 노드
      */
-    static void postOrder(TreeNode node) {
-        if(node == null)
-            return;
-
-        postOrder(node.getLeft());
-        postOrder(node.getRight());
-        System.out.println(node.getData());
-    }
+    public void postOrder(TreeNode node);
 
 
 }
